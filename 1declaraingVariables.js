2ae5456728f61c694499f91471d2a71d797f3445
viniciuskurt <viniciuskurt@hotmail.com>
2021-07-21 20:00:05 -0300

    Update 1declaraingVariables.js

--- 1declaraingVariables.js
+++ 1declaraingVariables.js
@@ -4,4 +4,4 @@ var name = 'Vinicius'
 var surname = 'Almeida'
 var age = 36
 
-console.log('My name is: ' + name + ' ' + surname + ' , age: ')
+console.log('My name is: ' + name + ' ' + surname +', age: ' + age);
